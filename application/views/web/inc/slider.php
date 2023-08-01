34f1f8865d61c0fc3159bcf324cdb5dece5d7b1d
fx466 <101171259+fx466@users.noreply.github.com>
2023-08-01 14:39:11 +0930

    further fixes(reload database file needed*)

--- application/views/web/inc/slider.php
+++ application/views/web/inc/slider.php
@@ -1,25 +1,5 @@
 <div class="header_bottom">
 
-        $popular_posts_chunk = array_chunk($popular_posts, 2);
-        foreach ($popular_posts_chunk as $single_popular_chunk) {
-            ?>
-            <div class="section group">
-                <?php foreach($single_popular_chunk as $single_popular){?>
-                <div class="listview_1_of_2 images_1_of_2">
-                    <div class="listimg listimg_2_of_1">
-                        <a href="<?php echo base_url('single/'.$single_popular->commodity_id);?>"> <img src="<?php echo base_url() ?>uploads/<?php echo $single_popular->commodity_image?>" alt="" /></a>
-                    </div>
-                    <div class="text list_2_of_1">
-                        <h2><?php echo word_limiter($single_popular->commodity_title,2) ?></h2>
-                        <p><?php echo word_limiter($single_popular->commodity_summary, 5) ?></p>
-                        <div class="button"><span><a href="<?php echo base_url('single/'.$single_popular->product_id);?>">Add to cart</a></span></div>
-                    </div>
-                </div>
-                <?php }?>
-            </div>
-        <?php } ?>
-        <div class="clear"></div>
-    </div>
     <div class="header_bottom_right_images">
         <!-- FlexSlider -->
         <section class="slider">
@@ -43,12 +23,11 @@
     .header_bottom {
         display: flex;
         justify-content: center;
-        width: 90%; /* Adjust the width as needed */
-        margin: 0 auto; /* Center the header_bottom horizontally */
+        width: 90%; /* Adjust the width as needed /
+        margin: 0 auto; / Center the header_bottom horizontally /
     }
 
     .header_bottom_right_images {
-        /* Add any additional styles specific to this section if needed */
+        / Add any additional styles specific to this section if needed */
     }
 </style>
-
