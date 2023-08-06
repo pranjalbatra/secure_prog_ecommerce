edc6dc1590c996738b8803f9d44b689c218de339
fx466 <101171259+fx466@users.noreply.github.com>
2023-08-06 12:17:15 +0930

    deleted backend folder

--- backend/table_creation.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS sche_1;
-USE sche_1;
-
-CREATE TABLE IF NOT EXISTS first_table (
-    user_id INT AUTO_INCREMENT,
-    user_name VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
-    PRIMARY KEY (user_id)
-);
