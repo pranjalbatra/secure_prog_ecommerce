03d759d3a6e0cd4453fce4625b37832f064e9f73
fx466 <101171259+fx466@users.noreply.github.com>
2023-08-07 19:38:36 +0930

    modifying db table

--- application/models/Slider_model.php
+++ application/models/Slider_model.php
@@ -5,13 +5,13 @@ class Slider_Model extends CI_Model
 
     public function save_slider_info($data)
     {
-        return $this->db->insert('tbl_slider', $data);
+        return $this->db->insert('sed_slider', $data);
     }
 
     public function getall_slider_info()
     {
         $this->db->select('*');
-        $this->db->from('tbl_slider');
+        $this->db->from('sed_slider');
         $info = $this->db->get();
         return $info->result();
     }
@@ -19,7 +19,7 @@ class Slider_Model extends CI_Model
     public function edit_slider_info($id)
     {
         $this->db->select('*');
-        $this->db->from('tbl_slider');
+        $this->db->from('sed_slider');
         $this->db->where('slider_id', $id);
         $info = $this->db->get();
         return $info->row();
@@ -28,27 +28,27 @@ class Slider_Model extends CI_Model
     public function delete_slider_info($id)
     {
         $this->db->where('slider_id', $id);
-        return $this->db->delete('tbl_slider');
+        return $this->db->delete('sed_slider');
     }
 
     public function update_slider_info($data, $id)
     {
         $this->db->where('slider_id', $id);
-        return $this->db->update('tbl_slider', $data);
+        return $this->db->update('sed_slider', $data);
     }
 
     public function published_slider_info($id)
     {
         $this->db->set('upload_status', 1);
         $this->db->where('slider_id', $id);
-        return $this->db->update('tbl_slider');
+        return $this->db->update('sed_slider');
     }
 
     public function unpublished_slider_info($id)
     {
         $this->db->set('upload_status', 0);
         $this->db->where('slider_id', $id);
-        return $this->db->update('tbl_slider');
+        return $this->db->update('sed_slider');
     }
 
 }
