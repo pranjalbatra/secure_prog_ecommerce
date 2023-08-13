e49b08552e8b0ce996c1dc7bcaa4a1f826ea0e29
pranjalbatra22 <82360833+pranjalbatra22@users.noreply.github.com>
2023-08-13 14:34:17 +0930

    security helper update

--- application/helpers/security_helper.php
+++ application/helpers/security_helper.php
@@ -1,5 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+defined('MAX_LENGTH') OR define('MAX_LENGTH', 255);
 
 if (!function_exists('sanitizeData')) {
     function sanitizeData($arr) {
@@ -17,13 +18,16 @@ if (!function_exists('sanitizeData')) {
     }
 
     function cleanInput($input) {
+
         // Remove potential HTML, JavaScript, and PHP tags
         $input = strip_tags($input);
-
         // Escape special characters to prevent SQL injection
         $input = addslashes($input);
-
-        return $input;
+        // Truncate input to a safe length to prevent buffer overflow
+        $sanitizedInput = substr($input, 0, MAX_LENGTH);
+        // Remove potentially harmful characters
+        $sanitizedInput = filter_var($sanitizedInput, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
+        return $sanitizedInput;
     }
 }
 
